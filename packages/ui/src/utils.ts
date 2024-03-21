f0da71dc46169f4e3fa829a2d1a870470d2a8d9c
quangnmwork <quangnm.work@gmail.com>
2024-03-21 15:38:30 +0900

    initial commit

---
+++ packages/ui/src/utils.ts
@@ -0,0 +1,9 @@
+/**
+ * @description Function for calc sum
+ * @param {number} a First value
+ * @param {string} b Second value
+ * @returns {Number} Sum of two value
+ */
+export function calc(a: number, b: number): number {
+  return a + b;
+}
